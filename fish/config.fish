466f971eea59c19ba1afa896b82bd0db19227e1a
ThDag <tahadoktor@gmail.com>
2023-09-06 14:45:48 +0300

    random stuff

--- fish/config.fish
+++ fish/config.fish
@@ -27,3 +27,13 @@ alias ls='exa --long --git --icons --no-user --header -F'
 alias lsa='exa --long --git --icons --no-user --header -F -a'
 alias ll='exa --icons -F'
 alias lla='exa --icons -F -a'
+
+
+set -gx TERM xterm-256color
+
+# theme
+set -g theme_color_scheme terminal-dark
+set -g fish_prompt_pwd_dir_length 1
+set -g theme_display_user yes
+set -g theme_hide_hostname no
+set -g theme_hostname always
